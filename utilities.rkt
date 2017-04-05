8bd5f707afd01094f975354ec1abc8dd9584b6e4
Jono Chadwell <Jono.Chadwell@gmail.com>
2017-04-04 17:51:20 -0700

    Smallest set utility method

--- utilities.rkt
+++ utilities.rkt
@@ -1,5 +1,7 @@
 #lang typed/racket
 
+(require typed/rackunit)
+
 (: pair (All (A B) (-> A B (Pairof A B))))
 (define pair cons)
 
@@ -17,4 +19,18 @@
 (define (sum-list lst)
   (foldl + (first lst) (rest lst)))
 
-(provide pair left right min-list sum-list)
+(: smallest-set (All (A) (-> (Listof (Setof A)) (Setof A))))
+(define (smallest-set lst)
+  (if (empty? lst)
+      (error "no smallest element of empty list")
+      (if (empty? (rest lst))
+          (first lst)
+          (let ([next (smallest-set (rest lst))])
+            (if (<= (set-count (first lst)) (set-count next))
+                (first lst)
+                next)))))
+
+(check-equal? (smallest-set (list (set 0 1 2) (set 0 1) (set 0 1 2 3)))
+              (set 0 1))
+
+(provide pair left right min-list sum-list smallest-set)
