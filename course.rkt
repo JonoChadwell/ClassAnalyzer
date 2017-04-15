c4eadf1c15cbe644c0fd1e69d145c315cb9dfd90
Jono Chadwell <Jono.Chadwell@gmail.com>
2017-04-15 14:27:47 -0700

    Added framework for cannonical course names. Broke everything

---
+++ course.rkt
@@ -0,0 +1,57 @@
+#lang typed/racket
+
+(require "types.rkt")
+
+(: get-first-alphabetically (-> course-id course-id course-id))
+(define (get-first-alphabetically a b)
+  (if (string-ci<? (course-id-dept a) (course-id-dept b))
+      a
+      b))
+
+;; gets the cannonical id of a course
+(: course-identifier (-> course course-id))
+(define (course-identifier crs)
+  (let ([lst (set->list (course-identifiers crs))])
+    (if (empty? lst)
+        (error "Course has no attached identifiers")
+        (foldl get-first-alphabetically (first lst) (rest lst)))))
+
+;; gets the cannonical department of a course
+(: course-dept (-> course String))
+(define (course-dept crs)
+  (course-id-dept (course-identifier crs)))
+
+;; gets the cannonical number of a course
+(: course-number (-> course String))
+(define (course-number crs)
+  (course-id-number (course-identifier crs)))
+
+(module+ test
+  (require typed/rackunit)
+
+  (define test-id-1 (course-id "CPE" "357"))
+  (define test-id-2 (course-id "CSC" "357"))
+
+  (define test-course-1
+    (course
+     (set test-id-1 test-id-2)
+     4
+     "Systems Programming"
+     (all-of (list
+              (exactly (course-id "CPE" "103"))
+              (one-of (list
+                       (exactly (course-id "CPE" "225"))
+                       (exactly (course-id "CPE" "233"))))))))
+
+  (check-equal?
+   (get-first-alphabetically test-id-1 test-id-2)
+   test-id-1)
+
+  (check-equal?
+   (course-number test-course-1)
+   "357")
+
+  (check-equal?
+   (course-dept test-course-1)
+   "CPE"))
+
--- types.rkt
+++ types.rkt
@@ -1,25 +1,39 @@
 #lang typed/racket
 
-;; represents a course
-;; Number may include _###, which is used to represent a "variant" of a course.
-;; this is done to explode requirements that use the same course twice into easier
-;; to manage requirements with no double duplicated courses.
-(struct course ([dept : String] [number : String]) #:transparent)
+;; Represents an identifier which a course goes by. Crosslisted courses may have
+;; several such identifiers.
+(struct course-id
+  ([dept : String]
+   [number : String])
+  #:transparent)
+
+;; When a single cannonical name is needed for a crosslisted class the name and
+;; class number from the alphabetically first department is used. For courses
+;; which offer a range of units, units is assumed to be the maximal value
+;; (for now).
+(struct course
+  ([identifiers : (Setof course-id)]
+   [units : Integer]
+   [name : String]
+   [prereqs : Requirement])
+  #:transparent)
 
 ;; represents a group of courses
-(define-type course-set (Setof course))
+(define-type course-set (Setof course-id))
 
 ;; represents a course requirement, as e.g. a graduation requirement for a particular program
 (define-type Requirement (U exactly one-of all-of))
 
 ;; represents a requirement that a student take a particular course
-(struct exactly ([take : course]) #:transparent)
+(struct exactly ([take : course-id]) #:transparent)
 
 ;; represents a requirement that a student satisfy one of a set of requirements
 (struct one-of ([reqs : (Listof Requirement)]) #:transparent)
 
 ;; represents a requirement that a student satisfy all of a set of requirements
 (struct all-of ([reqs : (Listof Requirement)]) #:transparent)
+
+(define empty-requirement (all-of empty))
 
 ;; represents the full requirements for a degree
 (struct curriculum
@@ -47,9 +61,15 @@
  all-of?
  all-of-reqs
  Requirement
+ empty-requirement
+ course-id
+ course-id-dept
+ course-id-number
  course
- course-dept
- course-number
+ course-identifiers
+ course-units
+ course-name
+ course-prereqs
  course-set
  student
  student-id
